06470cfa6a2f78acc027e61a315c1465ca4f4f64
havidtech <76119744+havidtech@users.noreply.github.com>
2022-03-17 06:52:33 +0100

    fix: update erc20 token

--- contracts/SimpleERC20.sol
+++ contracts/SimpleERC20.sol
@@ -5,41 +5,32 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-
 contract SimpleERC20 is ERC20, Ownable {
 
     using SafeMath for uint256;
-    uint private immutable maxSupply;
-    uint private immutable tokenPerEther;
-    uint private constant ETHER_TO_WEI = 1000000000000000000;
+    uint private constant maxSupply = 1000000 * 10 ** decimals();
 
 
-    constructor () ERC20("SimpleERC20", "SERC20"){
-        maxSupply = 1000000 * 10 ** decimals();
-        tokenPerEther = 1000 * 10 ** decimals();
-    }
+    constructor () ERC20("SimpleERC20", "SERC20"){}
 
+    // 1 ether equals 1000 tokens in the major currency of this token
+    // Since this token uses 18 decimals,
+    // in the minor currency, 1 wei equals tokens
+    // No conversions are needed since msg.value is already in wei
     function buyToken(address reciever) public payable {
-        require(valueIsWholeNumberEther(msg.value), "ether must be a whole number" );
-        uint amount = valueToToken(msg.value);
-        require(maxSupplyNotReached(amount), "Total Supply limit Reached");
-        _mint(reciever, amount);
-    }
+        require(msg.value > 0, "You didn't provide any ether");
+        require(maxSupplyNotReached(msg.value), "Total Supply limit Reached");
+        _mint(reciever, msg.value);
 
-    function valueIsWholeNumberEther(uint value) internal pure returns(bool) {
-        return value % ETHER_TO_WEI == 0;
-    }
-
-    function valueToToken(uint value) internal view returns (uint) {
-        return (value / ETHER_TO_WEI) * tokenPerEther;
+        withdrawPayment(msg.value);
     }
 
     function maxSupplyNotReached(uint amount) internal view returns (bool) {
         return amount + totalSupply() <= maxSupply;
     }
 
-    function withdraw() public onlyOwner {
-        (bool sent, bytes memory data) = payable(owner()).call{value: address(this).balance}("");
+    function withdrawPayment(uint _value) public internal {
+        (bool sent, bytes memory data) = payable(owner()).call{value: _value}("");
         require(sent, "Failed to send Ether");
     }
 }
